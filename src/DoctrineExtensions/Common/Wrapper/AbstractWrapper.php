a389365bbffc9a46ab85da1035219002c1ff20de
Daniel Chodusov <daniel@chodusov.com>
2020-10-02 17:47:56 +0200

    Renamed package

--- src/DoctrineExtensions/Common/Wrapper/AbstractWrapper.php
+++ src/DoctrineExtensions/Common/Wrapper/AbstractWrapper.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the DoctrineExtensions package.
+ * This file is part of the arodax/doctrine-extensions-common package.
  *
  * (c) ARODAX  <info@arodax.com>
  *
